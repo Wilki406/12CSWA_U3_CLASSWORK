de4fa91e881792285e7d4d6743ae79bd7944e754
Benjamin <134766715+Wilki406@users.noreply.github.com>
2024-03-12 22:52:03 +1100

    Update SelectionSort.py

--- Algorithms/SelectionSort.py
+++ Algorithms/SelectionSort.py
@@ -1,1 +1,25 @@
+elements = []
 
+def selectionSort(elements):
+    for i in range(len(elements)): # iterate through the length of elements
+        smallest = i # assume current value is the lowest
+        for k in range(i + 1, len(elements)): # iterate through the unsorted portion of the list to find the minimum
+            if elements[k] < elements[smallest]: # if the current element is < than the element update min index
+                smallest = k # swap current element with lowest element
+        if smallest != i: #
+            temp = elements[smallest]
+            elements[smallest] = elements[i]
+            elements[i] = temp
+    print(elements)
+    return elements
+
+
+
+
+
+
+
+
+
+
+selectionSort(elements)
